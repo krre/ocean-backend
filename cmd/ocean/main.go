3332582bac954c3fbecf2a95b4515e294894d9cf
Vladimir Zarypov <krre31@gmail.com>
2020-02-02 09:16:50 +0200

    Add http server with test handler on 'append' request

--- cmd/ocean/main.go
+++ cmd/ocean/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Ocean started")
+
+	appendHandler := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		io.WriteString(w, "OK\n")
+		fmt.Println("appendHandler")
+	}
+
+	http.HandleFunc("/append", appendHandler)
+	log.Fatal(http.ListenAndServe(":11000", nil))
 }
