69f0dd315a419ac5732bd038b5ee37678f0a1979
Vladimir Zarypov <krre31@gmail.com>
2020-03-04 08:47:23 +0200

    Add 'postgres' crate to dependencies

--- src/ocean/db.rs
+++ src/ocean/db.rs
@@ -1,9 +1,18 @@
 use crate::config;
+use postgres::{Client, NoTls};
 
 pub struct Db {}
 
 impl Db {
     pub fn new(config: &config::Postgres) -> Db {
+        let client = Client::configure()
+            .host("localhost")
+            .port(config.port)
+            .dbname(&config.database)
+            .user(&config.username)
+            .password(&config.password)
+            .connect(NoTls)
+            .unwrap();
         Db {}
     }
 }
