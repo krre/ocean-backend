c4c90a4b9f120ed24de16f9a94c255a62f117b67
Vladimir Zarypov <krre31@gmail.com>
2020-03-15 11:36:30 +0200

    Cleanup comments

--- src/ocean/api_server.rs
+++ src/ocean/api_server.rs
@@ -9,8 +9,6 @@ pub struct ApiServer {
 }
 
 async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    // Always return successfully with a response containing a body with
-    // a friendly greeting ;)
     Ok(Response::new(Body::from("hello, world!")))
 }
 
@@ -24,21 +22,12 @@ impl ApiServer {
 
         let addr = ([127, 0, 0, 1], self.port).into();
 
-        // Create a server bound on the provided address
-        let serve_future = Server::bind(&addr)
-            // Serve requests using our `async serve_req` function.
-            // `serve` takes a closure which returns a type implementing the
-            // `Service` trait. `service_fn` returns a value implementing the
-            // `Service` trait, and accepts a closure which goes from request
-            // to a future of the response.
-            .serve(make_service_fn(|_| async {
-                {
-                    Ok::<_, hyper::Error>(service_fn(serve_req))
-                }
-            }));
+        let serve_future = Server::bind(&addr).serve(make_service_fn(|_| async {
+            {
+                Ok::<_, hyper::Error>(service_fn(serve_req))
+            }
+        }));
 
-        // Wait for the server to complete serving or exit with an error.
-        // If an error occurred, print it to stderr.
         if let Err(e) = serve_future.await {
             eprintln!("server error: {}", e);
         }
