eafa2fd91caa5921628309c6887a907bd3490db2
Vladimir Zarypov <krre31@gmail.com>
2020-03-21 18:35:44 +0200

    Use router in processing request

--- src/ocean/api_server.rs
+++ src/ocean/api_server.rs
@@ -1,28 +1,27 @@
 use crate::router;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::Body;
-use hyper::Request;
-use hyper::Response;
 use hyper::Server;
 
 pub struct ApiServer {
     port: u16,
-    router: router::Router,
-}
-
-async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    Ok(Response::new(Body::from("hello, world!")))
 }
 
 impl ApiServer {
     pub fn new(port: u16) -> ApiServer {
-        let router = router::Router::new();
-        ApiServer { port, router }
+        ApiServer { port }
     }
 
     pub async fn listen(&self) {
         let addr = ([127, 0, 0, 1], self.port).into();
-        let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(serve_req)) });
+
+        let service = make_service_fn(|_| async {
+            // let router = router.clone();
+            Ok::<_, hyper::Error>(service_fn(move |req| async {
+                let router = router::Router::new();
+                router.route(req)
+            }))
+        });
+
         let server = Server::bind(&addr).serve(service);
 
         println!("API server listen on port {}", self.port);
--- src/ocean/router.rs
+++ src/ocean/router.rs
@@ -1,7 +1,14 @@
+use hyper::{Body, Request, Response};
+
 pub struct Router {}
 
 impl Router {
     pub fn new() -> Router {
         Router {}
+    }
+
+    pub fn route(self, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+        println!("{}", req.uri().path());
+        Ok(Response::new(Body::from("hello, world!")))
     }
 }
