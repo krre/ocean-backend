fa125be36e828068c60ca7df174c1ccbd8626669
Vladimir Zarypov <krre31@gmail.com>
2020-03-22 09:52:48 +0200

    Add request error printing

--- src/ocean/router.rs
+++ src/ocean/router.rs
@@ -1,9 +1,12 @@
 use hyper::{Body, Method, Request, Response, StatusCode};
 
 pub fn route(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    println!("{}", req.uri().path());
-
     if req.method() != Method::POST || req.uri().path() != "/dive" {
+        println!(
+            "Bad request: method: {}, URL: {}",
+            req.method().as_str(),
+            req.uri().path()
+        );
         return Ok(Response::builder()
             .status(StatusCode::BAD_REQUEST)
             .body(Body::from("Bad request"))
