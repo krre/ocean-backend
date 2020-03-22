1017bc53d5a197e66bcf46b0f666a0851fb050f7
Vladimir Zarypov <krre31@gmail.com>
2020-03-22 09:43:46 +0200

    Add `/dive` request for base routing

--- src/ocean/router.rs
+++ src/ocean/router.rs
@@ -1,6 +1,14 @@
-use hyper::{Body, Request, Response};
+use hyper::{Body, Method, Request, Response, StatusCode};
 
 pub fn route(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     println!("{}", req.uri().path());
+
+    if req.method() != Method::POST || req.uri().path() != "/dive" {
+        return Ok(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::from("Bad request"))
+            .unwrap());
+    }
+
     Ok(Response::new(Body::from("hello, world!")))
 }
