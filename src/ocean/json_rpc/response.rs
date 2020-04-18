76c2ab8636ede6232934d857204262cc671a7dcb
Vladimir Zarypov <krre31@gmail.com>
2020-04-18 11:46:49 +0300

    Skip serialize struct fields if value is None

--- src/ocean/json_rpc/response.rs
+++ src/ocean/json_rpc/response.rs
@@ -5,7 +5,9 @@ use serde_json;
 pub struct Response {
     pub id: String,
     pub method: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub result: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<Error>,
 }
 
@@ -13,5 +15,6 @@ pub struct Response {
 pub struct Error {
     pub code: i32,
     pub message: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub data: Option<serde_json::Value>,
 }
