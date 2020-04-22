5f946b1c7d5cd938af04ea65e0ac97908e73a8ba
Vladimir Zarypov <krre31@gmail.com>
2020-04-22 19:50:31 +0300

    Add JSON-RPC error codes

--- src/ocean/json_rpc/response.rs
+++ src/ocean/json_rpc/response.rs
@@ -1,5 +1,29 @@
 use serde::Serialize;
 use serde_json;
+use std::collections::HashMap;
+
+pub type ErrorCode = i32;
+
+// Common (1..99)
+pub const PARSE_ERROR: ErrorCode = 1;
+pub const CONTROLLER_NOT_FOUND: ErrorCode = 2;
+pub const METHOD_NOT_FOUND: ErrorCode = 3;
+pub const PARAMETER_NOT_FOUND: ErrorCode = 4;
+
+// User (100..199)
+pub const INVALID_PAIR_ID_PASSWORD_: ErrorCode = 100;
+
+lazy_static! {
+    static ref ERROR_MESSAGES: HashMap<ErrorCode, &'static str> = {
+        let mut m = HashMap::new();
+        m.insert(PARSE_ERROR, "Parse error");
+        m.insert(CONTROLLER_NOT_FOUND, "Controller not found");
+        m.insert(METHOD_NOT_FOUND, "Method not found");
+        m.insert(PARAMETER_NOT_FOUND, "Parameter not found");
+        m.insert(INVALID_PAIR_ID_PASSWORD_, "Invalid pair `id / password`");
+        m
+    };
+}
 
 #[derive(Serialize)]
 pub struct Response {
@@ -13,8 +37,18 @@ pub struct Response {
 
 #[derive(Serialize)]
 pub struct Error {
-    pub code: i32,
+    pub code: ErrorCode,
     pub message: String,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub data: Option<serde_json::Value>,
+    pub data: Option<String>,
+}
+
+impl Error {
+    pub fn new(code: ErrorCode, data: Option<String>) -> Error {
+        Error {
+            code: code,
+            message: ERROR_MESSAGES.get(&code).unwrap().to_string(),
+            data: data,
+        }
+    }
 }
