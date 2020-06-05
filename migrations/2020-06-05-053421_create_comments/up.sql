4bd6941b155c5fe94f9c256b945e34d2684f4a5c
Vladimir Zarypov <krre31@gmail.com>
2020-06-05 18:34:45 +0300

    Add `mandela_id` column into `comments` table

--- migrations/2020-06-05-053421_create_comments/up.sql
+++ migrations/2020-06-05-053421_create_comments/up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS comments (
     id serial NOT NULL PRIMARY KEY,
-    message text NOT NULL,
+    mandela_id INT NOT NULL REFERENCES mandels(id) ON DELETE CASCADE ON UPDATE CASCADE,
     user_id INT NOT NULL REFERENCES users(id) ON DELETE SET DEFAULT ON UPDATE CASCADE,
+    message text NOT NULL,
     create_ts timestamptz NOT NULL DEFAULT now(),
     update_ts timestamptz NOT NULL DEFAULT now()
 );
