5f03b8fc4f0191f71196f884f628052c5da1225f
Vladimir Zarypov <krre31@gmail.com>
2020-06-13 14:59:00 +0300

    Add insert indexes migration

---
+++ migrations/2020-06-13-114513_create_indexes/up.sql
@@ -0,0 +1,5 @@
+CREATE INDEX mandels_user_id_idx ON mandels (user_id);
+CREATE INDEX users_token_idx ON users (token);
+CREATE INDEX comments_mandela_id_idx ON comments (mandela_id);
+CREATE INDEX marks_mandela_id_idx ON marks (mandela_id);
+CREATE INDEX marks_user_id_idx ON marks (user_id);
