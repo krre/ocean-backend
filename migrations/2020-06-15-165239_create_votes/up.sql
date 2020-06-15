87f9f8f092c7ea25dc8f717f5f99d9246811cff6
Vladimir Zarypov <krre31@gmail.com>
2020-06-15 20:13:50 +0300

    Add creating indexes for `votes` table

--- migrations/2020-06-15-165239_create_votes/up.sql
+++ migrations/2020-06-15-165239_create_votes/up.sql
@@ -4,4 +4,8 @@ CREATE TABLE IF NOT EXISTS votes (
     user_id int NOT NULL REFERENCES users(id) ON DELETE SET DEFAULT ON UPDATE CASCADE,
     vote smallint NOT NULL DEFAULT 0,
     create_ts timestamptz NOT NULL DEFAULT now()
-)
+);
+
+CREATE INDEX votes_mandela_id_idx ON votes (mandela_id);
+CREATE INDEX votes_user_id_idx ON votes (user_id);
+CREATE INDEX votes_vote_idx ON votes (vote);
