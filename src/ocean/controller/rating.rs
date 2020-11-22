3a82b7a473139462730289b888d1ddcd3403f241
Vladimir Zarypov <krre31@gmail.com>
2020-11-22 16:08:22 +0200

    Add pagination fields to `rating.getUsers` response

--- src/ocean/controller/rating.rs
+++ src/ocean/controller/rating.rs
@@ -85,7 +85,16 @@ pub fn get_mandels(data: RequestData) -> RequestResult {
 
 // rating.getUsers
 pub fn get_users(data: RequestData) -> RequestResult {
+    #[derive(Deserialize)]
+    struct Req {
+        limit: i32,
+        offset: i32,
+    }
+
+    let req = serde_json::from_value::<Req>(data.params.unwrap())?;
+
     use diesel::prelude::*;
+    use diesel::sql_types::Int4;
     use diesel::sql_types::Int8;
     use diesel::sql_types::Text;
 
@@ -97,18 +106,44 @@ pub fn get_users(data: RequestData) -> RequestResult {
         count: i64,
     }
 
-    use diesel::dsl::*;
-
-    let list = sql_query(
+    let list = diesel::dsl::sql_query(
         "SELECT u.name, count(m.*)
         FROM users AS u
         INNER JOIN mandels as m on m.user_id = u.id
         GROUP BY u.name
         ORDER BY count DESC
-        LIMIT 50",
+        LIMIT $1
+        OFFSET $2",
     )
+    .bind::<Int4, _>(req.limit)
+    .bind::<Int4, _>(req.offset)
     .load::<User>(&data.db.conn)?;
 
-    let result = serde_json::to_value(&list)?;
+    #[derive(QueryableByName)]
+    struct UserCount {
+        #[sql_type = "Int8"]
+        count: i64,
+    }
+
+    let user_count = diesel::dsl::sql_query(
+        "SELECT count(*) FROM
+            (SELECT count(u.id) FROM users AS u
+            INNER JOIN mandels AS m ON m.user_id = u.id
+            GROUP BY u.id) AS user_count",
+    )
+    .load::<UserCount>(&data.db.conn)?;
+
+    #[derive(Serialize)]
+    struct Resp {
+        user_count: i64,
+        users: Vec<User>,
+    };
+
+    let resp = serde_json::to_value(&Resp {
+        user_count: user_count[0].count,
+        users: list,
+    })?;
+
+    let result = serde_json::to_value(&resp)?;
     Ok(Some(result))
 }
